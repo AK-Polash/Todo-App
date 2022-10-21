18915e2a998cb50e2a663174199a9725eb9de24b
AK-Polash <polashk199@gmail.com>
2022-10-22 02:04:34 +0600

    having problem to remove element from array by clicking the delete button

--- js/custom.js
+++ js/custom.js
@@ -1,20 +1,26 @@
 let input = document.querySelector(".input");
 let addButton = document.querySelector(".button");
 let ul = document.querySelector(".ul");
+let mainArr = [];
 
 addButton.addEventListener("click", () => {
   todo();
 });
 
 function todo() {
-  ul.innerHTML += `<li> ${input.value} <button class="delete"> Delete Task </button> </li>`;
+  ul.innerHTML += `<li> <span> ${input.value} </span> <button class="delete"> Delete Task </button> </li>`;
+  let listValue = ul.lastElementChild.children[0].innerHTML;
+  mainArr.push(listValue);
+  console.log(mainArr);
 
   let removeButton = document.querySelectorAll(".delete");
   let buttonArr = Array.from(removeButton);
 
-  buttonArr.map((button) => {
+  buttonArr.map((button, index) => {
     button.addEventListener("click", () => {
       button.parentElement.remove();
+      mainArr.splice(index, 1);
+      console.log(mainArr);
     });
   });
 }
